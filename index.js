1d0b41571cefdf47d06b0c5ff7082dd615ba6ebe
boris <boris.jeletich@outlook.com>
2022-01-16 18:21:52 +0100

    ✨ Added all API endpoints

---
+++ index.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+
+exports.getPosts = async function() {
+    result = await axios.get('https://borisjeletich.nl/v1/api/post/read.php')
+        .then((response) => {
+            return response.data;
+        });
+    return result;
+}
+
+exports.getPost = async function(id) {
+    result = await axios.get('https://borisjeletich.nl/v1/api/post/read_single.php?id=' + id)
+        .then((response) => {
+            return response.data;
+        });
+    return result;
+}
+
+exports.createBase = async function(apikey) {
+    result = await axios.post('https://borisjeletich.nl/v1/api/post/create.php', {}, { headers: { 'token': apikey, 'Content-Type': 'application/json' } })
+        .then((response) => {
+            return response.data;
+        });
+    return result;
+}
+
+exports.createPost = async function(apikey, id, title, description, category, images, version) {
+    result = await axios.post('https://borisjeletich.nl/v1/api/post/createRest.php', { id: id, name: title, description: description, category: category, images: images, version: version }, { headers: { 'token': apikey, 'Content-Type': 'application/json' } })
+        .then((response) => {
+            return response.data;
+        });
+    return result;
+
+}
---
+++ test.js
@@ -0,0 +1,21 @@
+const schemapi = require('./index.js');
+
+async function main() {
+    let result = await schemapi.getPosts();
+    console.log(result);
+
+    //schemapi.getPost(id);
+    result = await schemapi.getPost(2);
+    console.log(result);
+
+
+    result = await schemapi.createBase('your-api-key');
+    console.log(result);
+
+    //schemapi.createPost('your-api-key', id, title, description, category, images, version);
+    result = await schemapi.createPost('your-api-key', id , 'test', 'test', 'test', 'test', '123');
+    console.log(result);
+
+}
+
+main();
