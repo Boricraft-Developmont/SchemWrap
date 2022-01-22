a2f65dc8788150eb1a5088f3dfc5ed76d1374120
boris <boris.jeletich@outlook.com>
2022-01-22 11:34:31 +0100

    🐛 Removed json stringify function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 exports.getPosts = async function() {
     result = await axios.get('https://borisjeletich.nl/v1/api/post/read.php')
         .then((response) => {
-            return JSON.stringify(response.data);
+            return response.data
         });
     return result;
 }
@@ -12,7 +12,7 @@ exports.getPosts = async function() {
 exports.getPost = async function(id) {
     result = await axios.get('https://borisjeletich.nl/v1/api/post/read_single.php?id=' + id)
         .then((response) => {
-            return JSON.stringify(response.data);
+            return response.data
         });
     return result;
 }
@@ -20,7 +20,7 @@ exports.getPost = async function(id) {
 exports.createBase = async function(apikey) {
     result = await axios.post('https://borisjeletich.nl/v1/api/post/create.php', {}, { headers: { 'token': apikey, 'Content-Type': 'application/json' } })
         .then((response) => {
-            return JSON.stringify(response.data);
+            return response.data
         });
     return result;
 }
@@ -28,7 +28,7 @@ exports.createBase = async function(apikey) {
 exports.createPost = async function(apikey, id, title, description, category, images, version) {
     result = await axios.post('https://borisjeletich.nl/v1/api/post/createRest.php', { id: id, name: title, description: description, category: category, images: images, version: version }, { headers: { 'token': apikey, 'Content-Type': 'application/json' } })
         .then((response) => {
-            return JSON.stringify(response.data);
+            return response.data
         });
     return result;
 
@@ -37,7 +37,7 @@ exports.createPost = async function(apikey, id, title, description, category, im
 exports.searchPosts = async function(search) {
     result = await axios.get('https://borisjeletich.nl/v1/api/post/search.php?search=' + search)
         .then((response) => {
-            return JSON.stringify(response.data);
+            return response.data
         });
     return result;
 }
